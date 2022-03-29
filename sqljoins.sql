1f51ce6f876f02eb3532be9f74467d6436f81e23
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-03-29 19:29:20 +0530

    [mahafuz] added functions, joins & triggers

---
+++ sqljoins.sql
@@ -0,0 +1,149 @@
+create database Sales
+
+/*---- Use Database ----*/
+use Sales
+
+/*---- Create Table in Database ----*/
+Create table Customer
+(
+	Customer_Id int Identity(10,1) Primary key,
+	Customer_Name varchar(255),
+	Customer_Mobile bigint,
+	Address varchar(255),
+	City varchar(255),
+	Postal_Code bigint,
+	Country varchar(255),
+	Gender Char null Default 'M'
+	
+);
+
+/*---- Insert some dummy data in Table ----*/
+insert into Customer 
+values ('john', 2323121, 'newyork', 'newyork', 742374,'India', 'M'),
+('joe', 56778899, 'california', 'california', 56466,'USA', 'M'),
+('rashmi', 242344, 'London', 'London',677756,'UK', 'F'),
+('marshall', 23234345, 'kathmandu', 'kathmandu', 688866, 'Nepal', 'M'),
+('bella', 5676577, 'bangalore', 'bangalore', 456577, 'India', 'F');
+
+/*---- Alter existing Table add new Column ----*/
+
+select * from Customer
+select Customer_Name From Customer
+select distinct Country from Customer;
+select * from Customer where Country = 'India';
+
+create table product
+(
+ Product_id int identity(10,1) primary key,
+ Product_name varchar(255) not null,
+ Price decimal not null,
+ Discount decimal not null,
+ NetPay decimal not null,
+ Customer_Id int FOREIGN KEY REFERENCES Customer(Customer_Id) on delete no action
+);
+
+drop table product;
+Insert into product
+values('samsung s10', 35000.00, 5000, 30000.00, 12),
+('dell s10', 45000.00, 2000, 43000.00, 13),
+('Redmi k20', 20000.00, 1000, 19000.00, 14),
+('Iphone 13', 85000.00, 3000, 82000.00, 11);
+
+select * from product;
+
+select Customer_Name, Product_Name, NetPay
+from Customer c
+Inner Join product p
+on c.Customer_Id = p.Product_id
+order by Customer_Name Asc;
+
+select Customer_Name, Product_Name
+from Customer c
+Inner Join product p
+on c.Customer_Id = p.Product_id
+order by Customer_Name Desc;
+
+select Customer_Name, Product_Name
+from Customer c
+Join product p
+on c.Customer_Id = p.Product_id
+order by Customer_Name Asc;
+
+select *
+from Customer c
+Join product p
+on c.Customer_Id = p.Product_id
+order by Customer_Name Asc;
+
+select *
+from Customer c
+Join product p
+on c.Customer_Id > p.Product_id
+order by Customer_Name Asc;
+
+
+/*----- Implement Left Join ------*/
+select *
+from Customer c
+Left Join product p
+on c.Customer_Id = p.Product_id
+
+select Customer_Name, Product_Name
+from Customer c
+Left Join Product p
+on c.Customer_Id = p.Product_Id
+order by Customer_Name Desc;
+
+select Customer_Name, Product_Name
+from Customer c
+Left Join Product p
+on c.Customer_Id = p.Product_Id
+order by Customer_Name Asc;
+
+/*----- Implement Right Join ------*/
+select Customer_Name, Product_Name
+from Customer c
+Right Join Product p
+on c.Customer_Id = p.Product_Id
+order by Customer_Name Asc;
+
+select Customer_Name, Product_Name
+from Customer c
+Right Join Product p
+on c.Customer_Id = p.Product_Id
+order by Customer_Name Desc;
+
+select *
+from Customer c
+Right Join Product p
+on c.Customer_Id = p.Product_Id
+order by Customer_Name Desc;
+
+/*----- Implement Full outer Join ------*/
+select *
+from Customer c
+Full outer Join Product p
+on c.Customer_Id = p.Product_Id
+order by Customer_Name Desc;
+
+select Customer_Name, Product_Name
+from Customer c
+Full Join Product p
+on c.Customer_Id = p.Product_Id
+order by Customer_Name Asc;
+
+select *
+from Customer c
+Full Join Product p
+on c.Customer_Id = p.Product_Id;
+
+
+/*----- Implement Cross Join ------*/
+select *
+from Customer c
+Cross Join Product p;
+
+select Customer_Name, Customer_Mobile, Product_name, Price ,Discount, NetPay
+from Customer c
+Cross join product p
+order by Customer_Name Asc;
---
+++ sqltriggers.sql
@@ -0,0 +1,87 @@
+use sales
+
+-- create a Table
+create Table Employees
+(
+	Employee_Id int identity(10,1) primary key,
+	Employee_Name varchar(Max),
+	Mobile bigint,
+	Email varchar(200),
+	Address varchar(Max),
+	Gender char,
+	Department varchar(Max)
+);
+
+-- create a Audit Table
+create table EmployeeAudit
+(
+	Id int,
+	Inserted_By varchar(Max),
+	Audit_Data varchar(Max)
+);
+
+/*--- Create Trigger for Insert ---*/
+Create Trigger Trg_Insert_Employees
+on Employees
+For Insert
+As
+Begin
+	Declare @Id int
+	select @Id = Employee_Id from inserted
+	Insert into EmployeeAudit (Id,Inserted_By,Audit_Data)
+	values(@Id, ORIGINAL_LOGIN(), 
+	'New Employee with Employee Id ='+ CAST(@Id as nvarchar(5))+ ' is added at '+ CAST(GETDATE() as nvarchar(20)))
+End;
+
+Insert Into Employees values('john',78563342378,'john4115@gmail.com','bangalore,karnataka', 'M','Software Developer');
+select * from EmployeeAudit;
+select * from Employees;
+
+
+/****** Drop a Trigger *********/
+DROP TRIGGER IF EXISTS Trg_Insert_Employee;
+
+/****** Trigger For Delete ******/
+Create or Alter Trigger Trg_Delete_Employees
+on Employees
+For Delete
+As
+Begin
+	Declare @Id int
+	select @Id = Employee_Id from deleted
+	Insert into EmployeeAudit (Id,Inserted_By,Audit_Data)
+	values(@Id, ORIGINAL_LOGIN(), 
+	'Employee with Employee Id ='+ CAST(@Id as nvarchar(5))+ ' is Deleted at '+ CAST(GETDATE() as nvarchar(20)))
+End;
+
+Delete from Employees where Employee_Id = 11;
+select * from EmployeeAudit;
+select * from Employees;
+
+/****** Trigger No Delete Permission ******/
+Create or Alter Trigger Trg_NoDelete_Employees
+on Employees
+For Delete
+As
+Begin
+	rollback
+	print '==================================';
+	print 'You do not have permission to delete';
+	print '==================================';
+End;
+
+/****** Trigger For Update ******/
+-- Basic Update Trigger
+Create or Alter Trigger Trg_Update_Employees
+on Employees
+For Update
+As
+Begin
+	select * from deleted;
+	select * from inserted;
+End;
+
+select * from EmployeeAudit;
+select * from Employees
+update Employees set Employee_Name = 'mahafuz ahmed' where Employee_Id = 11;
+
